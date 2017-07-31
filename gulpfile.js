539cbde11eb89ac16d5a1c66ab2e9e75bdd8c594
Jim Bennett <jim@advancebass.com>
2017-07-31 12:30:04 -0700

    updated workflow and refactoring code

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,26 @@
-require('./gulp/tasks/styles');
-require('./gulp/tasks/watch');
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');
+
+
+// File paths
+var CSS_BUILD_PATH = './assets/build/styles',
+    CSS_PATH = './assets/styles/**/*.scss';
+
+
+gulp.task('default', function() {
+    console.log('Starting gulp Styles task');
+
+    return gulp.src(CSS_PATH)
+        .pipe(autoprefixer())
+        .pipe(sass({
+            // outputStyle: 'compressed'
+        }))
+        // .pipe(sourcemaps.write())
+        // .on('error', function(errorInfo){
+        //     console.log(errorInfo.toString());
+        //     this.emit('end');
+        // })
+        .pipe(gulp.dest(CSS_BUILD_PATH))
+});
